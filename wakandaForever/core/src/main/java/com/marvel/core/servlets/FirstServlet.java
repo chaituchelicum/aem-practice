acd04673a1900b9e2d84d2b359b388931f7ef471
Chaitanya <chaitanya.chelicum@ktisystems.co.uk>
2023-03-12 11:05:31 +0000

    Servlet with json response

--- wakandaForever/core/src/main/java/com/marvel/core/servlets/FirstServlet.java
+++ wakandaForever/core/src/main/java/com/marvel/core/servlets/FirstServlet.java
@@ -1,11 +1,13 @@
 package com.marvel.core.servlets;
 
+import com.marvel.core.models.FirstServletResponse;
 import com.marvel.core.services.FirstService;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.servlets.HttpConstants;
 import org.apache.sling.api.servlets.SlingAllMethodsServlet;
 import org.apache.sling.servlets.annotations.SlingServletResourceTypes;
+import org.json.JSONObject;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.propertytypes.ServiceDescription;
@@ -19,7 +21,7 @@ import java.io.IOException;
 @SlingServletResourceTypes(
         resourceTypes="wakandaForever/components/helloworld",
         methods=HttpConstants.METHOD_GET,
-        extensions="txt")
+        extensions="json")
 // To Test :- http://localhost:4502/content/wakandaForever/us/en/FirstPage/jcr:content/root/container/container/helloworld.txt
 public class FirstServlet extends SlingAllMethodsServlet {
 
@@ -28,7 +30,11 @@ public class FirstServlet extends SlingAllMethodsServlet {
 
     @Override
     protected void doGet(final SlingHttpServletRequest req, final SlingHttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text");
-        resp.getWriter().write("The name from servlet : "+firstService.getRandomName());
+        FirstServletResponse response = new FirstServletResponse();
+        response.setName(firstService.getRandomName());
+        response.setAge(20);
+        JSONObject json = new JSONObject(response);
+        resp.setContentType("json");
+        resp.getWriter().write(json.toString());
     }
 }
