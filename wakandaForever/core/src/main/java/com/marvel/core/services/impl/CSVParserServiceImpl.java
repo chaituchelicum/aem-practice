384ba0a521129ae8e59e566c00286c3a0bdad000
Chaitanya <chaitanya.chelicum@ktisystems.co.uk>
2023-07-17 20:12:55 +0100

    Creating the json file and clearing the csv folder

--- wakandaForever/core/src/main/java/com/marvel/core/services/impl/CSVParserServiceImpl.java
+++ wakandaForever/core/src/main/java/com/marvel/core/services/impl/CSVParserServiceImpl.java
@@ -5,9 +5,8 @@ import com.marvel.core.services.CSVParserService;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
-import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ResourceResolver;
-import org.apache.sling.api.resource.ResourceResolverFactory;
+import org.apache.jackrabbit.commons.JcrUtils;
+import org.apache.sling.api.resource.*;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.osgi.service.component.annotations.Component;
@@ -15,10 +14,12 @@ import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
 import java.io.BufferedReader;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 @Component(service = CSVParserService.class, immediate = true)
@@ -33,9 +34,7 @@ public class CSVParserServiceImpl implements CSVParserService {
     @Override
     public void parseCSV(String path) {
         logger.info("CSV PATH : {}",path);
-
         try {
-            // Create a map of authentication information.
             Map<String, Object> authenticationInfo = new HashMap<>();
             authenticationInfo.put("sling.service.subservice", "wakanda-service");
 
@@ -54,10 +53,51 @@ public class CSVParserServiceImpl implements CSVParserService {
                 jsonArray.put(jsonObject);
             }
             String jsonString = jsonArray.toString();
+            saveJsonFile(jsonString, resourceResolver);
+            deleteOnSuccess(path, resourceResolver);
             logger.info("JSON STRING : {}",jsonString);
             resourceResolver.close();
         }catch (Exception e){
             logger.error(e.getMessage());
+
         }
+    }
+
+    private void saveJsonFile(String json, ResourceResolver resourceResolver) {
+        try{
+            String targetFolderPath = "/content/dam/wakandaForever/jsonfolder";
+            String jsonFileName = "output.json";
+            Resource targetFolder = resourceResolver.getResource(targetFolderPath);
+            // Create the JSON file
+            String jsonFilePath = targetFolderPath + "/" + jsonFileName;
+            Session session = resourceResolver.adaptTo(Session.class);
+            Node targetNode = session.getNode(targetFolderPath);
+            // Create the nt:file node
+            Node fileNode = JcrUtils.getOrAddNode(targetNode, "students.json", "nt:file");
+            // Create the jcr:content node under the nt:file node
+            Node contentNode = JcrUtils.getOrAddNode(fileNode, "jcr:content", "nt:resource");
+            // Set the file's binary content
+            contentNode.setProperty("jcr:data", json);
+            // Set the file's MIME type (optional)
+            contentNode.setProperty("jcr:mimeType", "application/json");
+            session.save();
+            logger.info("JSON file created at: " + jsonFilePath);
+        }catch (Exception e){
+            logger.error(e.getMessage());
+        }
+    }
+
+    private void deleteOnSuccess(String path, ResourceResolver resourceResolver){
+        try {
+            Session session = resourceResolver.adaptTo(Session.class);
+            session.removeItem(path);
+            session.save();
+        }catch (Exception e){
+            logger.error(e.getMessage());
+        }
+
+    }
+    private void createCF(){
+
     }
 }
